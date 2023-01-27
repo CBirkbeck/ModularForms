d4b3135a978a2066fbd9f136e795b83d258b9a39
CBirkbeck <c.birkbeck@ucl.ac.uk>
2023-01-27 17:49:08 +0000

    more q-exp tests

--- src/mod_forms/discriminant_form.lean
+++ src/mod_forms/discriminant_form.lean
@@ -82,6 +82,14 @@ begin
 sorry,
 end
 
+lemma q_exp_iden_2 (k : ℕ) (hk : 3 ≤ k) (hk2: even k) (z : ℍ):
+∑' (x : ℤ × ℤ),  1/(((x.1 : ℂ)*z+x.2)^k) = 2 * (Riemann_zeta k) +
+  2 * (∑' (c : ℕ), (∑' (d : ℤ), 1/(c*z + d)^k)) :=
+begin
+rw Riemann_zeta,
+sorry,
+end
+
 
 def sigma_fn (k n : ℕ) : ℤ := ∑ (d : ℕ)  in nat.divisors n, d^k
 
@@ -94,17 +102,46 @@ simp_rw finset.sum_singleton,
 simp,
 end
 
+lemma sigma_fn_nonneg (k n: ℕ) : 0 ≤ sigma_fn k n :=
+begin
+rw sigma_fn,
+apply finset.sum_nonneg,
+intros i hi,
+apply pow_nonneg,
+linarith,
+end
 
-lemma Eisen_q_exp (k : ℕ) (hk : 3 ≤ k) (hk2 : even k) (z : ℍ) :
+lemma eisen_iden (k : ℕ) (hk : 3 ≤  (k : ℤ)) (hk2 : even k) (z : ℍ):
+(Eisenstein_series k) z = Eisenstein_series_of_weight_ k  z :=
+begin
+simp_rw Eisenstein_series,
+simp [hk],
+rw Eisenstein_series_is_modular_form,
+simp_rw Eisenstein_is_slash_inv,
+refl,
+end
+
+lemma Eisen_q_exp (k : ℕ) (hk : 3 ≤ (k : ℤ)) (hk2 : even k) (z : ℍ) :
   (Eisenstein_series k) z =  2* (Riemann_zeta k) +
   2 * ((-2 *  ↑π * I)^k/(k-1)!) * ∑' (n : ℕ),  (sigma_fn (k-1) (n+1))*(complex.exp ( 2 *↑π * I * z * n)) :=
 begin
+rw eisen_iden k hk hk2,
+rw Eisenstein_series_of_weight_,
+simp_rw Eise,
+norm_cast at hk,
+have:= q_exp_iden_2 k hk hk2 z,
+norm_cast,
+rw this,
+simp,
+have :=q_exp_iden k _ _,
+sorry,
+sorry,
 sorry,
 end
 
 lemma I_pow_4 : I^4 = 1 :=
 begin
-sorry,
+simp only [I_pow_bit0, neg_one_sq],
 end
 
 lemma embedding_coer : embedding (coe : ℝ → ℂ) :=
@@ -137,16 +174,10 @@ protected lemma coe_tsum { α : Type*} {f : α → ℝ} : summable f → ↑(tsu
 
 lemma coe_summable { α : Type*} (f : α → ℝ) : summable ((coe : ℝ → ℂ) ∘ f) ↔ summable f :=
 begin
-split,
-intro h,
-rw ←summable_norm_iff at h,
-rw ←summable_norm_iff,
-convert h,
-simp only [real.norm_eq_abs, norm_eq_abs, abs_of_real],
-intro h,
-rw ←summable_norm_iff at *,
-convert h,
-simp only [norm_eq_abs, abs_of_real, real.norm_eq_abs],
+  apply summable.map_iff_of_left_inverse complex.of_real complex.re_add_group_hom,
+  exact complex.continuous_of_real,
+  exact complex.continuous_re,
+  intro, refl,
 end
 
 
@@ -165,6 +196,25 @@ apply hf,
 
 end
 
+lemma auxeq (r : ℝ) (hr : 0 < r) : (r : ℂ) ≠ 0 :=
+begin
+  by_contra,
+  rw complex.ext_iff at h,
+  simp at h,
+  rw h at hr,
+  simp at hr,
+  exact hr,
+end
+
+lemma ineq : 0 ≤ 16 * π ^ 4 / ((2 + 1) * 2) :=
+begin
+apply div_nonneg,
+simp,
+apply pow_nonneg,
+apply real.pi_pos.le,
+linarith,
+end
+
 lemma Eisen_4_non_zero : G[(4 : ℕ)] ≠ 0 :=
 begin
 by_contra,
@@ -174,35 +224,42 @@ have H2 := H (⟨I, by {simp only [I_im, zero_lt_one]}⟩ : ℍ),
 have hk : 3 ≤ 4, by {linarith},
 have hk2 : even 4, by {simp only [even_bit0]},
 have H3 := Eisen_q_exp 4 hk hk2 (⟨I, by {simp}⟩ : ℍ),
-simp at H2,
+simp only [pi.zero_apply] at H2,
 
-have r1 : ((-2 *  ↑π * I)^4/(4-1)!)= (16 * π^4)/(3!), by {ring_exp, rw I_pow_4, simp,},
+have r1 : ((-2 *  ↑π * I)^4/(4-1)!)= (16 * π^4)/(3!), by {ring_exp, rw I_pow_4, simp only [one_mul],},
 rw r1 at H3,
 have r2 : ∀ (n : ℤ),
   complex.exp ( 2 *↑π * I * I * n) = complex.exp (-2 * π * n),
-by {intro n, simp only [neg_mul], congr,  sorry},
+by {intro n, simp only [neg_mul], congr, have : 2 * ↑π * I * I * ↑n = 2 * ↑π * (I * I) * ↑n, by {ring}, rw this, rw I_mul_I, ring,},
 simp only [nat.cast_bit0, algebra_map.coe_one, nat.factorial_succ, nat.factorial_two, nat.cast_mul, nat.cast_add,
   nat.succ_sub_succ_eq_sub, tsub_zero, subtype.coe_mk] at H3,
 have r3 : ∑' (n : ℕ),  ↑(sigma_fn (3) (n+1))*(complex.exp ( 2 *↑π * I * I * n)) =
   ∑' (n : ℕ),  (sigma_fn (3) (n+1))*(complex.exp ( -2 *↑π * n)),
-by{sorry},
+by{congr', funext, simp, left, convert (r2 n), ring},
 rw r3 at H3,
 norm_cast at H3,
 have H4 : 0 ≤ ∑' (n : ℕ), (↑(sigma_fn 3 (n + 1)) * real.exp (↑(-2 : ℤ) * π * ↑n)),
-by {apply tsum_nonneg, intro b, sorry,},
+by {apply tsum_nonneg, intro b, apply mul_nonneg, norm_cast, apply sigma_fn_nonneg, apply (real.exp_pos _).le,},
 
 have H5: 0 < 2 * Riemann_zeta 4,
-by {sorry},
-
-have H6 : 0 < (2 * Riemann_zeta 4) +
-  ((2 * (16 * π ^ 4 / ↑((2 + 1) * 2))))* ∑' (n : ℕ), (↑(sigma_fn 3 (n + 1)) * real.exp (↑(-2 : ℤ) * π * ↑n)),
-by {sorry},
+by {apply mul_pos, linarith, apply Riemann_zeta_pos, linarith,},
 
 let ε := (2 * Riemann_zeta 4) +
   ((2 * (16 * π ^ 4 / ↑((2 + 1) * 2))))* ∑' (n : ℕ), (↑(sigma_fn 3 (n + 1)) * real.exp (↑(-2 : ℤ) * π * ↑n)),
 
-have H4: G[(4 : ℕ)] (⟨I, by {simp only [I_im, zero_lt_one]}⟩ : ℍ) = ↑ε,
-  by {rw H3, simp, left, norm_cast, apply tsum_coe,},
-sorry,
+have H7: G[(4 : ℕ)] (⟨I, by {simp only [I_im, zero_lt_one]}⟩ : ℍ) = ↑ε,
+  by {rw H3, simp only [of_real_mul, of_real_bit0, nat.cast_mul, nat.cast_add, nat.cast_bit0,
+  algebra_map.coe_one, of_real_div, of_real_add,
+  int.cast_neg, int.cast_bit0, neg_mul, of_real_int_cast, of_real_exp, of_real_neg, of_real_nat_cast, add_right_inj,
+  mul_eq_mul_left_iff, mul_eq_zero, bit0_eq_zero, one_ne_zero, div_eq_zero_iff, pow_eq_zero_iff, nat.succ_pos',
+  of_real_eq_zero, false_or, or_false], left, norm_cast, apply tsum_coe,},
 
+ have H5: 0 < ε,
+ by{ apply left.add_pos_of_pos_of_nonneg H5, apply mul_nonneg, simp, apply ineq, apply H4,
+ },
+
+have H8:=auxeq ε H5,
+rw ←H7 at H8,
+apply absurd H8,
+simpa using H2,
 end
